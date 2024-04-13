761ab225f29bef1ec0f6ae52979d95fec4c66743
tamandrew <andrew.c.tam@stonybrook.edu>
2024-04-13 11:48:38 -0400

    Create user login panel

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,19 @@ export default function App() {
     const { type, payload } = action;
     switch (type) {
       case UserActionType.SWITCH:
-        return { ...state, currentUser: payload };
+        if (state.users.some((user) => user.name === payload.name)) {
+          return { ...state, currentUser: payload };
+        } else {
+          console.log("User not found")
+          return state;
+        }
       case UserActionType.ADD:
-        return { ...state, users: [...state.users, payload] };
+        if (state.users.some((user) => user.name === payload.name)) {
+          console.log("User already exists");
+          return state;
+        } else {
+          return { ...state, users: [...state.users, payload] };
+        }
       default:
         return state;
     }
--- src/user/Login/Login.tsx
+++ src/user/Login/Login.tsx
@@ -1,28 +1,62 @@
-import { useContext } from "react"
-import "./Login.scss"
-import { DispatchContext, UserContext } from "../../App"
+import { useContext, useState } from "react";
+import "./Login.scss";
+import { DispatchContext, UserContext } from "../../App";
 import { UserActionType } from "../../types";
+import { names, uniqueNamesGenerator } from "unique-names-generator";
 
 export default function Login() {
-    const userState = useContext(UserContext);
-    const dispatch = useContext(DispatchContext);
+  const userState = useContext(UserContext);
+  const dispatch = useContext(DispatchContext);
 
-    return (
-        <div className = "login">
-            {userState.currentUser ? userState.currentUser.name : "No user logged in"}
+  const [nameInput, setNameInput] = useState("");
 
-            <button
-                onClick = {() => {
-                    dispatch({
-                        type: UserActionType.SWITCH,
-                        payload: {
-                            name: "User 1"
-                        }
-                    })
-                }}
-            >
-                Switch
-            </button>
-        </div>
-    )
+  const switchUser = (name: string) => {
+    dispatch({
+      type: UserActionType.SWITCH,
+      payload: { name },
+    });
+  };
+
+  const addUser = () => {
+    const name =
+      nameInput.length > 0
+        ? nameInput
+        : uniqueNamesGenerator({ dictionaries: [names] });
+
+    dispatch({
+      type: UserActionType.ADD,
+      payload: { name },
+    });
+
+    setNameInput("");
+  };
+
+  const loggedIn = userState.currentUser;
+
+  return (
+    <div className="login">
+      <div>
+        <input
+          className="userInput"
+          placeholder="Enter a username..."
+          value={nameInput}
+          onChange={(e) => setNameInput(e.target.value)}
+        />
+
+        <button onClick={addUser}>Add User</button>
+      </div>
+
+      <div>
+        {loggedIn ? `Logged in as ${loggedIn.name}` : "No user logged in"}
+      </div>
+      <div>Click on a user to login as them:</div>
+      <div className="userList">
+        {userState.users.map((user) => (
+          <div key={user.name}>
+            <button onClick={() => switchUser(user.name)}>{user.name}</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
